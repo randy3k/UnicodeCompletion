c070aec529b0979c383fb90d7acb4b4ae95c6492
Randy Lai <randy.cs.lai@gmail.com>
2016-07-08 12:18:02 -0400

    better emoji support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is a simple package to insert Unicode characters (both latex and emoji symbols) to Julia as in Julia REPL.
 For some reasons, [Julia Completions](https://github.com/jakeconnor/JuliaCompletions) doesn't quite
 work for me and [UnicodeMath](https://github.com/mvoidex/UnicodeMath)
-keymaping is different from Julia (e.g., `\epsilon` and `\varepsilon`), so I
+keymapping is different from Julia (e.g., `\epsilon` and `\varepsilon`), so I
 created this package.
 The list of unicodes is copied from
 [Julia](https://github.com/JuliaLang/julia/), check [latex_symbols.jl](latex_symbols.jl) and [emoji_symbols.jl](emoji_symbols.jl). 
@@ -17,4 +17,9 @@ Type `\alpha` and hit enter/tab to insert the corresponding unicode `α` to Subl
 
 #### Known issues for emoji:
 
-It is known that there is a bug in handling emoji, i.e., the text `\:` is not replaced. I have no plan to fix it for now.
+It is known that there is a bug in handling emoji. For example, when you hit
+enter while trying to insert `🍰` via `\:cake:`, the text `\:` is not replaced.
+It could probably be fixed by defining custom tab completion keybind. A
+workaround for now is to type the full text `\:cake:` and then trigger
+`auto_complete` (<kbd>ctrl+space</kbd> for Mac and Windows, <kbd>alt+/</kbd>
+for Linux).
--- julia-unicode.py
+++ julia-unicode.py
@@ -9,15 +9,22 @@ class LatexPlusAutoCompletions(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         if not view.match_selector(locations[0], "source.julia"):
             return None
-        if not prefix:
-            return
+
         ploc = locations[0]-len(prefix)
         if view.substr(sublime.Region(ploc-1, ploc)) == "\\":
             prefix = view.substr(sublime.Region(ploc-1, locations[0]))
         elif view.substr(sublime.Region(ploc-2, ploc)) == "\\^":
             prefix = view.substr(sublime.Region(ploc-1, locations[0]))
         elif view.substr(sublime.Region(ploc-2, ploc)) == "\\:":
-            prefix = view.substr(sublime.Region(ploc-1, locations[0]))
-
+            prefix = view.substr(sublime.Region(ploc-2, locations[0]))
+        elif view.substr(sublime.Region(ploc-1, ploc)) == ":":
+            pt = view.word(ploc-2).begin()
+            print(view.substr(sublime.Region(pt-2, pt))) == "\:"
+            prefix = view.substr(sublime.Region(pt-2, locations[0]))
+        else:
+            return None
+        if not prefix:
+            return None
+        print(prefix)
         r = latex_symbols + emoji_symbols
-        return [(s[0] + "\t" + s[1], s[1], s[1] ) for s in r if prefix in s[0]]
+        return [(s[0] + "\t" + s[1], prefix, s[1] ) for s in r if prefix in s[0]]
