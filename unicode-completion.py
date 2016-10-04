d59cdce8fa53ffe4eb591e7e9229631eddb4d786
Randy Lai <randy.cs.lai@gmail.com>
2016-10-04 19:46:49 -0400

    improve completion fix

--- unicode-completion.py
+++ unicode-completion.py
@@ -20,6 +20,15 @@ def get_prefix(view):
         return None
 
 
+def fix_completion(view, edit):
+    for sel in view.sel():
+        pt = sel.begin()
+        if view.substr(sublime.Region(pt-3, pt-1)) == "\\:":
+            view.replace(edit, sublime.Region(pt-3, pt-1), "")
+        elif view.substr(sublime.Region(pt-4, pt-1)) == "\\:+":
+            view.replace(edit, sublime.Region(pt-4, pt-1), "")
+
+
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
@@ -65,10 +74,7 @@ class UnicodeCompletionInsertBestCompletion(sublime_plugin.TextCommand):
             prefix = get_prefix(view)
             self.completions = [s[1] for s in symbols if s[0].startswith(prefix)]
             view.run_command("insert_best_completion",  {"default": "\t", "exact": False})
-            for sel in view.sel():
-                pt = sel.begin()
-                if view.substr(sublime.Region(pt-3, pt-1)) == "\\:":
-                    view.replace(edit, sublime.Region(pt-3, pt-1), "")
+            fix_completion(view, edit)
         else:
             region = sublime.Region(view.sel()[0].begin()-1, view.sel()[0].begin())
             prev_char = view.substr(region)
@@ -87,20 +93,14 @@ class UnicodeCompletionAutoComplete(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         view.run_command("auto_complete")
-        for sel in view.sel():
-            pt = sel.begin()
-            if view.substr(sublime.Region(pt-3, pt-1)) == "\\:":
-                view.replace(edit, sublime.Region(pt-3, pt-1), "")
+        fix_completion(view, edit)
 
 
 class UnicodeCompletionCommitComplete(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         view.run_command("commit_completion")
-        for sel in view.sel():
-            pt = sel.begin()
-            if view.substr(sublime.Region(pt-3, pt-1)) == "\\:":
-                view.replace(edit, sublime.Region(pt-3, pt-1), "")
+        fix_completion(view, edit)
 
 
 class UnicodeCompletionLookup(sublime_plugin.TextCommand):
