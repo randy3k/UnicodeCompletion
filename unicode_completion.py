9b1c08a99666e65e8f6fac568ee6cb1d82aeafac
Randy Lai <randy.cs.lai@gmail.com>
2021-07-13 17:47:28 -0700

    enable tab completion in widgets

--- unicode_completion.py
+++ unicode_completion.py
@@ -20,18 +20,8 @@ def normalize_completion(symbols):
 
 
 class UnicodeListener(UnicodeCompletionMixin, sublime_plugin.EventListener):
-
-    def should_complete(self, view, pt):
-        if view.settings().get("unicode_completion", False):
-            return True
-        elif view.settings().get('is_widget') and \
-                view.window().active_view().settings().get("unicode_completion", False):
-            return True
-        else:
-            return False
-
     def on_query_completions(self, view, prefix, locations):
-        if not self.should_complete(view, locations[0]):
+        if not view.settings().get("unicode_completion", False):
             return None
 
         prefix = self.look_command_backward(view, locations[0])
@@ -43,6 +33,49 @@ class UnicodeListener(UnicodeCompletionMixin, sublime_plugin.EventListener):
             ret = [s for s in emoji_symbols if s[0].startswith(prefix)]
 
         return normalize_completion(ret)
+
+    def on_query_context(self, view, key, operator, operand, match_all):
+        sel = view.sel()
+        if len(sel) == 0 or not sel[0].empty():
+            return
+
+        pt = sel[0].end()
+
+        if key == "active_view_setting.unicode_completion":
+            if view.settings().get('is_widget'):
+                return view.window().active_view().settings().get("unicode_completion", False)
+
+        elif key == 'unicode_completion_has_matches':
+            prefix = self.look_command_backward(view, pt)
+            return (prefix is not None) == operand
+
+        return None
+
+
+class UnicodeCompletionInsertBestCompletion(UnicodeCompletionMixin, sublime_plugin.TextCommand):
+    def run(self, edit, next_completion=False):
+        view = self.view
+        if len(view.sel()) == 0 or not view.sel()[0].empty():
+            return
+        pt = view.sel()[0].end()
+
+        if not next_completion:
+            prefix = self.look_command_backward(view, pt)
+            region = sublime.Region(view.sel()[0].begin()-len(prefix), view.sel()[0].begin())
+            exact_match = [s[1] for s in symbols if s[0] == prefix]
+            self.completions = exact_match + \
+                    list(set([s[1] for s in symbols if s[0].startswith(prefix) and s[0] != prefix]))
+            view.replace(edit, region, self.completions[0])
+        else:
+            region = sublime.Region(view.sel()[0].begin()-1, view.sel()[0].begin())
+            prev_char = view.substr(region)
+            if prev_char in self.completions:
+                prev_index = self.completions.index(prev_char)
+                next_index = prev_index + 1 if prev_index < len(self.completions) - 1 else 0
+                for sel in reversed(view.sel()):
+                    pt = sel.begin()
+                    if view.substr(sublime.Region(pt-1, pt)) == prev_char:
+                        view.replace(edit, sublime.Region(pt-1, pt), self.completions[next_index])
 
 
 class ToggleUnicodeCompletion(sublime_plugin.WindowCommand):
